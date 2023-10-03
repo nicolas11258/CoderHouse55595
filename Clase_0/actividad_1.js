eee0a0234b3a2d882163ad898fc4df2cb9ed4d31
nagudelo <nagudelo@payvalida.com>
2023-10-03 13:22:45 -0500

    Anexando codigo visto en clase

---
+++ Clase_0/actividad_1.js
@@ -0,0 +1,23 @@
+// Definir variables
+var nombre = "Juan";
+var edad = 30;
+var precio = 19.99;
+var series = ["Breaking Bad", "Game of Thrones", "Stranger Things"];
+var peliculas = ["Pulp Fiction", "The Shawshank Redemption", "Inception"];
+
+// Mostrar los valores por consola
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Precio:", precio);
+console.log("Series:", series);
+console.log("Películas:", peliculas);
+
+// Incrementar la edad en 1
+edad++;
+
+// Agregar una serie a la lista
+series.push("The Mandalorian");
+
+// Mostrar las variables actualizadas por consola
+console.log("Edad incrementada en 1:", edad);
+console.log("Lista de series actualizada:", series);
---
+++ Clase_0/ejemplo_1.js
@@ -0,0 +1,13 @@
+// Ejemplos de console.log(), console.error(), console.warn() y console.clear()
+
+// Utilizando console.log() para imprimir un mensaje
+console.log("Este es un mensaje de registro.");
+
+// Utilizando console.error() para imprimir un error
+console.error("Esto es un mensaje de error.");
+
+// Utilizando console.warn() para imprimir una advertencia
+console.warn("¡Cuidado! Esto es una advertencia.");
+
+// Utilizando console.clear() para limpiar la consola
+console.clear();
