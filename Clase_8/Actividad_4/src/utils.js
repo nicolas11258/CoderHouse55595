482476b83fe992964c6eb0a02efb02a25f0c5a5a
nagudelo <nagudelo@payvalida.com>
2023-10-24 18:21:44 -0500

    Clase #8

--- Clase_8/Actividad_4/src/utils.js
+++ Clase_8/Actividad_4/src/utils.js
@@ -3,17 +3,14 @@ import multer from 'multer';
 // Configurar la carpeta y el nombre del archivo
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Carpeta donde se guardarán los archivos
+    cb(null, './src/public/img'); // Carpeta donde se guardarán los archivos
   },
   filename: (req, file, cb) => {
     const timestamp = Date.now();
     const originalname = file.originalname;
-    const filename = `${timestamp}-${originalname}`;
-    console.log(filename, originalname)
+    const filename = `${timestamp}-Clase_8`;
     cb(null, filename);
   },
 });
 
-const uploader = multer({ storage: storage });
-
-export { uploader };
+export const uploader = multer({ storage });;
