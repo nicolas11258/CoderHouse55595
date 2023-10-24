b8080ec039a293da95a0e60cf9da6789afba2dd6
nagudelo <nagudelo@payvalida.com>
2023-10-24 17:48:47 -0500

    Clase #8

--- Clase_8/Middleware/src/app.js
+++ Clase_8/Middleware/src/app.js
@@ -1,58 +1,26 @@
 import express from 'express';
-import cors from 'cors';
 
 const app = express();
 const port = 3000;
+app.use(express.urlencoded({ extended: true }));
 
-// Middleware a nivel de aplicación
-const loggerMiddleware = (req, res, next) => {
-  console.log(`Solicitud recibida en: ${new Date()}`);
-  next();
-};
-
-app.use(loggerMiddleware);
-
-// Middleware a nivel de endpoint
-const endpointMiddleware = (req, res, next) => {
-  console.log('Middleware de nivel de endpoint');
-  next();
-};
-
-app.get('/ruta', endpointMiddleware, (req, res) => {
-  res.send('Ruta específica');
-});
-
-// Middleware a nivel del Router
-const router = express.Router();
-
-router.use((req, res, next) => {
-  console.log('Middleware de nivel de Router');
-  next();
-});
-
-router.get('/subruta', (req, res) => {
-  res.send('Subruta específica');
-});
-
-app.use('/api', router);
-
-// Middleware de manejo de errores
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send('Ha ocurrido un error en el servidor');
-});
-
-// Middleware incorporado
 app.use(express.json());
 
-// Middleware de terceros
-app.use(cors());
-
-// Ruta principal
-app.get('/', (req, res) => {
-  res.send('¡Aplicación Express con middleware!');
-});
-
 app.listen(port, () => {
-  console.log(`Servidor Express escuchando en el puerto ${port}`);
+    console.log(`Servidor Express escuchando en el puerto ${port}`);
+  });
+
+
+app.post('/users', authMiddleware, (req, res) => {
+  res.status(200).json({
+    user: req.body.user,
+  });
 });
+
+function authMiddleware(req, res, next) {
+  if (!req.body.auth) {
+    res.status(401).send('No autorizado');
+    return;
+  }
+  next();
+}
