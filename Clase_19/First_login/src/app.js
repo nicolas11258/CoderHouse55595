d082b53ed1aa11e5af4dccc0851a401047dde4f0
nagudelo <nagudelo@payvalida.com>
2023-11-30 17:57:40 -0500

    Clase #19

---
+++ Clase_19/First_login/src/app.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import handlebars from 'express-handlebars';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import MongoStore from 'connect-mongo';
+
+import __dirname from './utils.js';
+import indexRouter from './routes/views/index.js';
+import loginRouter from './routes/views/login.js';
+import registerRouter from './routes/views/register.js';
+import profileRouter from './routes/views/profile.js';
+import sessionsApiRouter from './routes/api/sessions.js';
+
+const app = express();
+const port = 8080;
+
+// Conexión a la base de datos MongoDB
+mongoose.connect("mongodb+srv://nicolas11258:0kCg8T4wNf5Ne0zc@cluster0.sy13hmr.mongodb.net/clase_18?retryWrites=true&w=majority", {});
+
+// Configuración de Handlebars
+app.engine('handlebars',handlebars.engine());
+app.set('views',__dirname+'/views')
+app.set('view engine','handlebars');
+
+// Configuración de middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Configuración de middleware para manejar sesiones usando connect-mongo
+app.use(session({
+    secret: 'CoderSecret', // Clave secreta para firmar las cookies de sesión
+    resave: false, // Evitar que se guarde la sesión en cada solicitud
+    saveUninitialized: true, // Guardar la sesión incluso si no se ha modificado
+    store: MongoStore.create({
+        mongoUrl:"mongodb+srv://nicolas11258:0kCg8T4wNf5Ne0zc@cluster0.sy13hmr.mongodb.net/clase_18?retryWrites=true&w=majority",
+
+        ttl: 2 * 60, // Tiempo de vida de la sesión en segundos (2 minutos en este caso)
+    }),
+}));
+
+// Configuración de rutas
+app.use('/', indexRouter);
+app.use('/', loginRouter);
+app.use('/', registerRouter);
+app.use('/', profileRouter);
+app.use('/api/sessions', sessionsApiRouter);
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal!');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando por el puerto: ${port}`);
+});
+
+export default app;
--- Clase_19/First_login/src/controllers/authController.js
+++ Clase_19/First_login/src/controllers/authController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/user.model.js";
 
-const registerUser = async (req, res) => {
+export const registerUser = async (req, res) => {
     try {
         // Lógica para registrar un usuario en la base de datos
         const { first_name, last_name, email, age, password } = req.body;
@@ -13,8 +13,15 @@ const registerUser = async (req, res) => {
     }
 };
 
-const loginUser = (req, res) => {
-    // Lógica para autenticar al usuario y crear la sesión
-    const { email, password } = req.body;
-    res.send('Usuario autenticado exitosamente');
+export const loginUser = async (req, res) => {
+    try {
+        // Lógica para autenticar al usuario y crear la sesión
+        const { email, password } = req.body;
+        // Implementa la lógica de autenticación aquí
+        // ...
+        res.send('Usuario autenticado exitosamente');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al autenticar usuario');
+    }
 };
