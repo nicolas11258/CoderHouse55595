5e3a8ca10bf2aeaadf2d575f4b6e5c94a31e0f67
Edwin Sepulveda <esepulveda@payvalida.com>
2023-12-11 20:25:04 -0500

    Passport avanzado

--- Clase_22/ejercicio_1/src/app.js
+++ Clase_22/ejercicio_1/src/app.js
@@ -1,22 +1,58 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-
-const PRIVATE_KEY = "CoderKeyFeliz";
+import { authToken, generateToken } from "./utils.js";
+import handlebars from "express-handlebars";
 
 const app = express();
 app.listen(8080, () => console.log("Listening on 8080"));
 
 app.use(express.json());
 
-app.post("/login", (req, res) => {
+app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
+
+app.use(express.static("./src/public"));
+const users = [];
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/api/register", (req, res) => {
+  const { name, email, password } = req.body;
+  const exists = users.find((user) => user.email === email);
+  if (exists)
+    return res
+      .status(400)
+      .send({ status: "error", error: "User already exists" });
+  const user = {
+    name,
+    email,
+    password,
+  };
+  users.push(user);
+  const access_token = generateToken(user);
+  res.send({ status: "success", access_token });
+});
+
+app.post("/api/login", (req, res) => {
   const { email, password } = req.body;
-  console.log(req.body);
   if (email === "user@gmail.com" && password === "12345") {
-    let token = jwt.sign({ email, password }, PRIVATE_KEY, {
-      expiresIn: "12h",
-    });
-    res.send({ message: "Login Success", token: token });
+    let token = generateToken(req.body);
+    res.send({ message: "Login Success", access_token: token });
   } else {
-    res.send({ message: "Error login", error: "User-password bad" });
+    res.send({ message: "Error", error: "Error" });
   }
+});
+
+app.get("/api/current", authToken, (req, res) => {
+  res.send({ status: "success", payload: req.user });
 });
---
+++ Clase_22/ejercicio_1/src/public/js/login.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  const obj = {};
+  data.forEach((value, key) => (obj[key] = value));
+  fetch("/api/login", {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((result) => result.json())
+    .then((json) => {
+      console.log(json);
+      localStorage.setItem("token", json.access_token);
+    });
+});
